c85f01f9cd5bf15ee7e6bad9a2cf48fbc18dae2c
Jamie Cafirma <jamie.cafirma@gmail.com>
2021-09-08 16:57:17 -0700

    Define a function that takes a single journal entry object and returns a DOM tree that matches one of the example entries in the HTML. Use a loop to create a DOM tree for each journal entry in the data model and append it to the page when the 'DOMContentLoaded' event is fired. Remove the example HTML entries once the user's entries are successfully being appended to the DOM on page load. #2

--- js/main.js
+++ js/main.js
@@ -30,3 +30,53 @@ function getFormData(event) {
 }
 
 $diaryEntryForm.addEventListener('submit', getFormData);
+
+/* <li class="row entry-margin">
+  <div class="column-half">
+    <img src="" alt="dummy entry 1" class="border-radius-4">
+  </div>
+  <div class="column-half">
+    <h2 class="image-title-style"></h2>
+    <p></p>
+  </div>
+</li> */
+
+function createEntry(entry) {
+  var $journalEntry = document.createElement('li');
+  $journalEntry.className = 'row entry-margin';
+
+  var $imagePlaceholder = document.createElement('div');
+  $imagePlaceholder.className = 'column-half';
+  $journalEntry.appendChild($imagePlaceholder);
+
+  var $entryImage = document.createElement('img');
+  $entryImage.className = 'border-radius-4';
+  $entryImage.setAttribute('src', entry.url);
+  $entryImage.setAttribute('alt', entry.title);
+  $imagePlaceholder.appendChild($entryImage);
+
+  var $entryText = document.createElement('div');
+  $entryText.className = 'column-half';
+  $journalEntry.appendChild($entryText);
+
+  var $entryTitle = document.createElement('h2');
+  $entryTitle.className = 'image-title-style';
+  $entryTitle.textContent = entry.title;
+  $entryText.appendChild($entryTitle);
+
+  var $entryNotes = document.createElement('p');
+  $entryNotes.textContent = entry.notes;
+  $entryText.appendChild($entryNotes);
+
+  return $journalEntry;
+}
+
+var $entryList = document.querySelector('#entry-list');
+
+function loadJournalEntries(event) {
+  for (var i = 0; i < data.entries.length; i++) {
+    $entryList.prepend(createEntry(data.entries[i]));
+  }
+}
+
+document.addEventListener('DOMContentLoaded', loadJournalEntries);
