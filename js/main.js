103e060d0f70a4d6425d4cb2f14c9b7e748a86e9
Jamie Cafirma <jamie.cafirma@gmail.com>
2021-09-09 11:06:41 -0700

    Ensure that submitting a new journal entry also creates a new DOM tree for it and adds it to the page. Ensure that submitting a new journal entry automatically shows the 'entries' view without reloading the page. #2

--- js/main.js
+++ js/main.js
@@ -10,37 +10,6 @@ function updateImage(event) {
 
 $photoUrl.addEventListener('input', updateImage);
 
-var $diaryEntryForm = document.querySelector('#diary-entry-form');
-
-function getFormData(event) {
-  event.preventDefault();
-
-  var formData = {};
-
-  formData.title = $diaryEntryForm.title.value;
-  formData.url = $diaryEntryForm.url.value;
-  formData.notes = $diaryEntryForm.notes.value;
-
-  formData.entryId = data.nextEntryId;
-  data.nextEntryId++;
-  data.entries.unshift(formData);
-
-  $photoPreview.setAttribute('src', 'images/placeholder-image-square.jpg');
-  $diaryEntryForm.reset();
-}
-
-$diaryEntryForm.addEventListener('submit', getFormData);
-
-/* <li class="row entry-margin">
-  <div class="column-half">
-    <img src="" alt="dummy entry 1" class="border-radius-4">
-  </div>
-  <div class="column-half">
-    <h2 class="image-title-style"></h2>
-    <p></p>
-  </div>
-</li> */
-
 function createEntry(entry) {
   var $journalEntry = document.createElement('li');
   $journalEntry.className = 'row entry-margin';
@@ -71,11 +40,51 @@ function createEntry(entry) {
   return $journalEntry;
 }
 
+var $diaryEntryForm = document.querySelector('#diary-entry-form');
+
 var $entryList = document.querySelector('#entry-list');
 
+function addNewEntry(event) {
+  event.preventDefault();
+
+  var formData = {};
+
+  formData.title = $diaryEntryForm.title.value;
+  formData.url = $diaryEntryForm.url.value;
+  formData.notes = $diaryEntryForm.notes.value;
+
+  formData.entryId = data.nextEntryId;
+  data.nextEntryId++;
+  data.entries.unshift(formData);
+
+  $photoPreview.setAttribute('src', 'images/placeholder-image-square.jpg');
+  $diaryEntryForm.reset();
+
+  $entryList.prepend(createEntry(formData));
+
+  var $entriesView = document.querySelector('#entries-view');
+  var $formView = document.querySelector('#form-view');
+
+  $entriesView.className = 'view';
+  $formView.className = 'view hidden';
+}
+
+$diaryEntryForm.addEventListener('submit', addNewEntry);
+
+/* <li class="row entry-margin">
+  <div class="column-half">
+    <img src="" alt="dummy entry 1" class="border-radius-4">
+  </div>
+  <div class="column-half">
+    <h2 class="image-title-style"></h2>
+    <p></p>
+  </div>
+</li> */
+
 function loadJournalEntries(event) {
+  $entryList = document.querySelector('#entry-list');
   for (var i = 0; i < data.entries.length; i++) {
-    $entryList.prepend(createEntry(data.entries[i]));
+    $entryList.appendChild(createEntry(data.entries[i]));
   }
 }
 
